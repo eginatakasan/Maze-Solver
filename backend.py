1ae78c572d70c956da23865a0f242c28715cfabf
viviannitjan <viviannitjan@yahoo.com>
2019-03-29 16:28:41 +0700

    perbaiki bfs

--- backend.py
+++ backend.py
@@ -2,6 +2,18 @@ import math
 #ketentuan file:
 #karena kalo enter dibaca
 #jadi baris terakhir juga harus dienter
+class Node():
+    def __init__(self,x,y,parent_x,parent_y):
+        self.x = x
+        self.y=y
+        self.parent_x=parent_x
+        self.parent_y=parent_y
+    
+    def search(self, q, x, y):
+        for n in q:
+            if(n.x==x and n.y==y):
+                return n
+            
 
 #read maze from file
 def readMaze():
@@ -11,48 +23,47 @@ def readMaze():
         matrix.append(list(x)[:-1])
     return matrix
 
-def bfs(matrix, visited, queue, start_x, start_y, finish_x, finish_y):
-    arr = [start_x, start_y]
+def bfs(matrix, visited, start_x, start_y, finish_x, finish_y):
+    start = Node(start_x,start_y,None,None)
+    queue=[]
     queue_visited = []
-    queue.append(arr)
+    queue.append(start)
     visited[start_x][start_y]=True
-    while (queue[0][0]!=finish_x or queue[0][1]!=finish_y) :
+    while queue:
         arr = queue[0]
         # print(arr)
         queue.pop(0)
         queue_visited.append(arr)
-        if (arr[1]-1>0): #kiri posisi sekarang
-            if(visited[arr[0]][arr[1]-1] == False):
-                # print("kiri")
-                visited[arr[0]][arr[1]-1] = True
-                x = [arr[0], arr[1]-1]
-                queue.append(x)
-        if (arr[1]+1<len(visited)): #kanan posisi sekarang
-            if(visited[arr[0]][arr[1]+1] == False):
-                # print("kanan")
-                visited[arr[0]][arr[1]+1] = True
-                x = [arr[0], arr[1]+1]
-                queue.append(x)
-        if (arr[0]-1>0): #atas posisi sekarang
-            if(visited[arr[0]-1][arr[1]] == False):
-                # print("atas")
-                visited[arr[0]-1][arr[1]] = True
-                x = [arr[0]-1, arr[1]]
-                queue.append(x)
-        if (arr[0]+1<len(visited[0])): #bawah posisi sekarang
-            if(visited[arr[0]+1][arr[1]] == False):
-                # print("bawah")
-                visited[arr[0]+1][arr[1]] = True
-                x = [arr[0]+1, arr[1]]
-                queue.append(x)
-        # print(queue)
-    arr = [finish_x, finish_y]
-    queue_visited.append(arr)
-    # print(queue_visited)
-    return queue_visited
+
+        if arr.x==finish_x and arr.y==finish_y:
+            path=[]
+            current = arr
+            while current.parent_x is not None:
+                # print("jalan")
+                path.append((current.x,current.y))
+                # print(current.x, current.y, current.parent_x, current.parent_y)
+                x=current.parent_x
+                y=current.parent_y
+                current = current.search(queue_visited, x, y)
+                # print(current.x, current.y, current.parent_x, current.parent_y)
+                # print (path[::-1])
+            path.append((current.x,current.y))
+            return path[::-1]
+
+        for pos in [(0,-1), (0,1), (-1,0), (1,0)]:
+            x = arr.x+pos[0]
+            y = arr.y+pos[1]
+            if x<0 or x>=len(visited[0]):
+                continue
+            if y<0 or y>=len(visited):
+                continue
+            if visited[x][y]==False:
+                visited[x][y]=True
+                child = Node(x,y,arr.x,arr.y)
+                queue.append(child)
 
 def countDistance(x, y, finish_x, finish_y):
-    return math.sqrt(10*(finish_x-x) + (finish_y-y)**2)
+    return abs(finish_x-x) + abs(finish_y-y)
 
 def astar(matrix, visited, queue, start_x, start_y, finish_x, finish_y):
     queue.append((start_x, start_y, 0, 0, 0))
@@ -99,9 +110,9 @@ if __name__ == "__main__":
                 visited1[i][j]=True
                 visited2[i][j]=True
     # print(visited)
-    queue = []
+    # queue = []
     #bfs
-    # queue_visited = bfs(matrix, visited1, queue, 1, 0, 9, 10)
-    # print(queue_visited)
+    queue_visited = bfs(matrix, visited1, 1, 0, 9, 10)
+    print(queue_visited)
     #astar
-    astar(matrix, visited2, queue, 1, 0, 9, 10)
+    # astar(matrix, visited2, queue, 1, 0, 9, 10)
